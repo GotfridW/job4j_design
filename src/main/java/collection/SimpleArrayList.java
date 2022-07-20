90bc96237a2a513fb45b57c78beb8de057aca77f
Gotfrid <gotfridwallenstein@gmail.com>
2022-07-21 02:52:03 +0300

    Task "Dynamic list on an array [#157]" completed

---
+++ src/main/java/collection/SimpleArrayList.java
@@ -0,0 +1,82 @@
+package collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    private T[] expand() {
+        container = Arrays.copyOf(container, container.length * 2);
+        return container;
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            container = expand();
+        }
+        container[size] = value;
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T result = container[index];
+        container[index] = newValue;
+        return result;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T result = container[index];
+        System.arraycopy(container, index + 1, container,
+                index, size - index - 1);
+        container[size - 1] = null;
+        size--;
+        modCount++;
+        return result;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int cursor = 0;
+            private int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[cursor++];
+            }
+        };
+    }
+}
