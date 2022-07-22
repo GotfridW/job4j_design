576292aa3a70f927d6cd09ccfa2140cf06260ded
Gotfrid <gotfridwallenstein@gmail.com>
2022-07-22 22:07:19 +0300

    Task [159#] on creating singly linked list completed.

---
+++ src/main/java/collection/SimpleLinkedList.java
@@ -0,0 +1,76 @@
+package collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+    private Node<E> head;
+    private int size;
+    private int modCount;
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value);
+        Node<E> current = head;
+        if (current == null) {
+            head = newNode;
+        } else {
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> result = head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            Node<E> current = head;
+            Node<E> lastReturned;
+            private int expectedModCount = modCount;
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                lastReturned = current;
+                current = current.next;
+                cursor++;
+                return lastReturned.data;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        public Node(E data) {
+            this.data = data;
+        }
+    }
+}
