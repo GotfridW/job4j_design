e0d0f63880faa522d582f42bc1293c8ed5e3734e
Gotfrid <gotfridwallenstein@gmail.com>
2022-07-23 14:11:42 +0300

    Task [#160] - Two stacks-based queue.

--- src/main/java/collection/SimpleStack.java
+++ src/main/java/collection/SimpleStack.java
@@ -2,12 +2,19 @@ package collection;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<>();
+    private int size = 0;
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
