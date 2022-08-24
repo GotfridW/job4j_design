66278f7a1b2bfe9b37dac073ff6445efc6b19a67
Gotfrid <gotfridwallenstein@gmail.com>
2022-08-24 23:56:32 +0300

    Task [#4898] - FileInputStream.

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.util.Arrays;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("Even.txt")) {
+            int i;
+            StringBuilder builder = new StringBuilder();
+            while ((i = in.read()) != -1) {
+                builder.append((char) i);
+            }
+            String[] str = builder.toString().split(System.lineSeparator());
+            Arrays.stream(str)
+                    .mapToInt(Integer::parseInt)
+                    .forEach(e -> System.out.println(e + " even: " + (e % 2 == 0)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
