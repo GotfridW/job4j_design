2005587648640ca87ab2c8d8b3cbf963a33a81da
Gotfrid <gotfridwallenstein@gmail.com>
2022-08-26 22:28:44 +0300

    Task [#858] - Reading configuration file

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comments.properties";
+        Config cfg = new Config(path);
+        cfg.load();
+        assertThat(cfg.value("name")).isEqualTo("Sherlock");
+    }
+
+    @Test
+    void whenPairsWithCommentsAndEmptyLines() {
+        String path = "./data/pairs_with_comments_and_empty_lines.properties";
+        Config cfg = new Config(path);
+        cfg.load();
+        assertThat(cfg.value("country")).isNotNull()
+                .isEqualTo("USA");
+        assertThat(cfg.value("city")).isEqualTo("Portland");
+    }
+
+    @Test
+    void whenLineHasMultipleEqualSigns() {
+        String path = "./data/pairs_with_multiple_equal_signs.properties";
+        Config cfg = new Config(path);
+        cfg.load();
+        assertThat(cfg.value("time")).isNotNull()
+                .isEqualTo("money=happiness");
+        assertThat(cfg.value("silence")).isEqualTo("gold=");
+    }
+
+    @Test
+    void whenLineWithoutValueThenThrowException() {
+        String path = "./data/line_without_value.properties";
+        Config cfg = new Config(path);
+        assertThatThrownBy(cfg::load).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+$")
+                .hasMessageContaining("Incorrect");
+        assertThat(cfg.value("shape")).isNotNull()
+                .isEqualTo("circle");
+    }
+
+    @Test
+    void whenLineHasNoEqualSignThenThrowException() {
+        String path = "./data/pair_without_equal_sign.properties";
+        Config cfg = new Config(path);
+        assertThatThrownBy(cfg::load).isInstanceOf(IllegalArgumentException.class);
+    }
+}
