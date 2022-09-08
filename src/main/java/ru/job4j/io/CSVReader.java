d479b910a9656604bd9aa3ab5bfeb5304f3e3d47
Gotfrid <gotfridwallenstein@gmail.com>
2022-09-09 01:01:54 +0300

    Task [#504791] - Scanner.

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,80 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class CSVReader {
+    public static void handle(ArgsName argsName) {
+        validate(argsName);
+        String out = argsName.get("out");
+        String delimiter = argsName.get("delimiter");
+        String filter = argsName.get("filter");
+
+        try (var scanner = new Scanner(new FileInputStream(argsName.get("path")));
+             var output = out.equals("stdout")
+                     ? new PrintStream(System.out)
+                     : new PrintStream(new FileOutputStream(out))) {
+             scanner.useDelimiter(System.lineSeparator());
+
+             var filters = Arrays.stream(filter.split(","))
+                     .toList();
+             var header = Arrays.stream(scanner.next().split(delimiter))
+                     .toList();
+             var indices = filters.stream()
+                     .mapToInt(header::indexOf)
+                     .filter(i -> i != -1)
+                     .boxed()
+                     .toList();
+
+             String filteredHeader = filterLine(header, indices, delimiter);
+             output.println(filteredHeader);
+             while (scanner.hasNext()) {
+                 var line = Arrays.asList(scanner.next().split(delimiter));
+                 var filteredLine = filterLine(line, indices, delimiter);
+                 output.println(filteredLine);
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+    }
+
+    private static String filterLine(List<String> line, List<Integer> indices, String delimiter) {
+        return indices.stream()
+                .map(line::get)
+                .collect(Collectors.joining(delimiter));
+    }
+
+    private static void validate(ArgsName argsName) {
+        Path path = Paths.get(argsName.get("path"));
+        Path out = Paths.get(argsName.get("out"));
+        if (!(Files.exists(path) && path.toFile().isFile())) {
+            throw new IllegalArgumentException(String.format(
+                    "Source file doesn't exist: %s", path));
+        }
+        if (!(Files.exists(out)) && out.toFile().isFile()) {
+            throw new IllegalArgumentException(String.format(
+                    "Output file doesn't exist: %s", out));
+        }
+        String delimiter = argsName.get("delimiter");
+        if (!delimiter.equals(";")) {
+            throw new IllegalArgumentException(String.format(
+                    "Delimiter must be \";\" but was %s", delimiter));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 4) {
+            throw new IllegalArgumentException(String.format(
+                    "Args must include 4 arguments but was: %s", args.length));
+        }
+        var argsName = ArgsName.of(new String[] {
+                args[0], args[1], args[2], args[3]});
+        handle(argsName);
+    }
+}
