700484fd735aaf50a844b1da4b6252471a6aeb08
Gotfrid <gotfridwallenstein@gmail.com>
2022-09-11 00:32:38 +0300

    Task [#268852] - Slf4j - variable output.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "John Locke";
+        int age = 35;
+        float heightInFeet = 6.8f;
+        double weight = 70.3;
+        LOG.debug("User info name : {}, age : {}, height : {}, weight : {}",
+                name, age, heightInFeet, weight);
+        boolean active = true;
+        short id = 42;
+        LOG.info("Profile info: active : {}, id : {}", active, id);
+        byte status = 2;
+        LOG.warn("Warning: current status : {}", status);
+        char code = 'F';
+        LOG.error("Error occurred! Code: {}", code);
     }
 }
