e51359f40b2606211a36bd9f9f63f3eae2e69af7
Gotfrid <gotfridwallenstein@gmail.com>
2022-09-10 23:04:04 +0300

    Task [#7921] - Bot

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,23 +3,25 @@ package ru.job4j.io;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
+        try (var server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
+                try (var out = socket.getOutputStream();
+                     var in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String msg = in.readLine();
-                    if (msg.contains("?msg=Bye")) {
-                        out.write("Bye".getBytes());
+                    if (msg.contains("?msg=Hello")) {
+                        out.write("Hello".getBytes());
+                    } else if (msg.contains("?msg=Exit")) {
                         server.close();
+                    } else if (msg.contains("?msg=")) {
+                        out.write("What".getBytes());
                     }
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
