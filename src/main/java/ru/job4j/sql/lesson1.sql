db3b47a05190adfacf7f81c0e6ea92783b9709f9
Gotfrid <gotfridwallenstein@gmail.com>
2022-10-14 00:51:32 +0300

    Task [#6860] - Database

---
+++ src/main/java/ru/job4j/sql/lesson1.sql
@@ -0,0 +1,16 @@
+﻿create table car(
+id serial primary key,
+brand text,
+prod_year varchar(4),
+cc decimal(2, 1),
+four_wheel_drive boolean
+);
+insert into car
+(brand, prod_year, cc, four_wheel_drive) 
+values
+('Toyota Mark 2', '2003', 2.5, FALSE);
+select * from car;
+update car set brand = 'Toyota Mark II', cc = 3.0;
+select * from car;
+delete from car;
+select * from car;
