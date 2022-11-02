efa54bbc9e49dc04818fd46a666a5b13b1934ace
Gotfrid <gotfridwallenstein@gmail.com>
2022-11-02 18:23:50 +0300

    Task [#1732] - Filters

---
+++ src/main/java/ru/job4j/sql/create_product.sql
@@ -0,0 +1,23 @@
+﻿create table type(
+id serial primary key,
+name varchar(255)
+);
+
+create table product(
+id serial primary key,
+name varchar(255),
+type_id int references type(id),
+expired_date date,
+price float
+);
+
+insert into type(name) values ('Сыр'), ('Молоко'), ('Колбасные изделия'), ('Соки');
+
+insert into product(name, type_id, expired_date, price) values ('Сыр Гауда', 1, '2022-11-10', 450);
+insert into product(name, type_id, expired_date, price) values ('Сыр плавленный', 1, '2022-12-25', 150);
+insert into product(name, type_id, expired_date, price) values ('Молоко 2,5%', 2, '2022-10-28', 90);
+insert into product(name, type_id, expired_date, price) values ('Кефир', 2, '2022-11-12', 110);
+insert into product(name, type_id, expired_date, price) values ('Мороженое Пломбир', 2, '2022-11-10', 45);
+insert into product(name, type_id, expired_date, price) values ('Мороженое шоколадное', 2, '2022-11-29', 56);
+insert into product(name, type_id, expired_date, price) values ('Колбаса Докторская', 3, '2022-10-31', 160);
+insert into product(name, type_id, expired_date, price) values ('Сок апельсиновый', 4, '2023-02-14', 80);
---
+++ src/main/java/ru/job4j/sql/select_product.sql
@@ -0,0 +1,29 @@
+﻿select * from product p
+join type t on t.id = p.type_id
+where t.name = 'Сыр';
+
+select * from product p
+where p.name like '%Мороженое%';
+
+select * from product p
+where p.expired_date < current_date;
+
+select * from product
+where price = (select max(price) from product);
+
+select t.name, count(*) as "Number of products"
+from product p
+join type t on t.id = p.type_id
+group by t.name;
+
+select * from product p
+join type t on t.id = p.type_id
+where t.name = 'Сыр' or t.name = 'Молоко';
+
+select t.name as "Product name", count(p.id) as "Number of products"
+from type t join product p on t.id = p.type_id
+group by t.name
+having count(p.id) < 10;
+
+select p.name, t.name from product p
+join type t on t.id = p.type_id;
