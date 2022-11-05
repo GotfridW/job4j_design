9fe13c5c0321ee71ec63bfd8266ebb7824c12c3b
Gotfrid <gotfridwallenstein@gmail.com>
2022-11-05 22:30:02 +0300

    Task [#6862] - Join

---
+++ src/main/java/ru/job4j/sql/couples.sql
@@ -0,0 +1,21 @@
+﻿create table teens(
+id serial primary key,
+name varchar,
+gender varchar(1)
+);
+
+insert into teens(name, gender) values('Alex', 'M');
+insert into teens(name, gender) values('Bob', 'M');
+insert into teens(name, gender) values('Felix', 'M');
+insert into teens(name, gender) values('George', 'M');
+insert into teens(name, gender) values('Mark', 'M');
+insert into teens(name, gender) values('Lucy', 'F');
+insert into teens(name, gender) values('Sara', 'F');
+insert into teens(name, gender) values('Rita', 'F');
+insert into teens(name, gender) values('Julia', 'F');
+insert into teens(name, gender) values('Nancy', 'F');
+
+select t1.name, t2.name
+from teens t1
+cross join teens t2
+where t1.gender != t2.gender and t1.gender = 'M';
---
+++ src/main/java/ru/job4j/sql/departments.sql
@@ -0,0 +1,57 @@
+﻿create table departments(
+id serial primary key,
+name varchar
+);
+
+create table employees(
+id serial primary key,
+name varchar,
+dept_id int references departments(id)
+);
+
+insert into departments(name) values ('strategy'), ('marketing'), ('finance'), ('HR'), ('technology');
+insert into employees(name, dept_id) values ('Frensis', 1);
+insert into employees(name, dept_id) values ('Sarah', 3);
+insert into employees(name, dept_id) values ('Murphy', 2);
+insert into employees(name, dept_id) values ('Nancy', 4);
+insert into employees(name, dept_id) values ('Andrew', 1);
+insert into employees(name, dept_id) values ('William', 3);
+insert into employees(name, dept_id) values ('Ann', 2);
+insert into employees(name, dept_id) values ('Daniel', 4);
+insert into employees(name, dept_id) values ('Alan', 4);
+insert into employees(name, dept_id) values ('Eric', null);
+
+select d.id, d.name, e.name 
+from departments d
+left join employees e on e.dept_id = d.id
+order by d.id asc;
+
+select d.id, d.name, e.name
+from departments d
+right join employees e on e.dept_id = d.id
+order by d.id asc;
+
+select d.id, d.name, e.name
+from departments d
+full join employees e on e.dept_id = d.id
+order by d.id asc;
+
+select d.id, d.name, e.name
+from departments d
+cross join employees e
+order by d.id asc;
+
+select d.id, d.name
+from departments d
+left join employees e on e.dept_id = d.id
+where e.dept_id is null;
+
+select e.name, d.name
+from employees e
+left join departments d on d.id = e.dept_id
+order by d.id asc;
+
+select e.name, d.name
+from departments d
+right join employees e on d.id = e.dept_id
+order by d.id asc;
