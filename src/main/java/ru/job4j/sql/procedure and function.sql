a0694d37e9215b04580dcb61cabe3217cc837d8e
Gotfrid <gotfridwallenstein@gmail.com>
2022-11-10 00:38:58 +0300

    Task [#504804] - Stored procedures and functions

---
+++ src/main/java/ru/job4j/sql/procedure and function.sql
@@ -0,0 +1,37 @@
+﻿create table products (
+id serial primary key,
+name varchar(50),
+producer varchar(50),
+count integer default 0,
+price integer
+);
+-- Используется таблица в БД к предыдущему заданию
+
+create or replace procedure delete_data (d_producer varchar)
+language 'plpgsql'
+as $$
+BEGIN
+delete from products 
+where producer = d_producer;
+END;
+$$;
+
+call delete_data ('Yamaha');
+
+create or replace function f_delete_data (d_count integer)
+returns integer
+language 'plpgsql'
+as $$
+declare
+result integer;
+begin
+select count(*) into result 
+from products 
+where count < d_count;
+delete from products 
+where count < d_count;
+return result;
+end;
+$$;
+
+select f_delete_data (4);
