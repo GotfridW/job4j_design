ad5a9acd03a39aca911ee870f1318af4c8b2a86c
Gotfrid <gotfridwallenstein@gmail.com>
2022-11-15 23:57:50 +0300

    Task [#504812] - Transactions. Isolation levels

---
+++ src/main/java/ru/job4j/sql/isol_levels.sql
@@ -0,0 +1,25 @@
+﻿create table fuel (
+id serial primary key,
+name varchar(20),
+volume integer,
+price float
+);
+
+insert into fuel (name, volume, price) values 
+('regular', 55, 3.8), ('mid-grade', 45, 4.2),
+('premium', 15, 4.55), ('diesel', 80, 5.4);
+
+--1st transaction:
+begin transaction isolation level serializable;
+select * from fuel;
+select * from fuel where volume = (select max(volume) from fuel);
+update fuel set volume = 50 where name = 'diesel';
+commit;
+
+--2nd transaction 
+
+begin transaction isolation level serializable;
+select * from fuel;
+select * from fuel where volume = (select max(volume) from fuel);
+update fuel set volume = 35 where name = 'regular';
+commit;
