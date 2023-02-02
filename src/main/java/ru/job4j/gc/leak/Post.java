6fe2edca4a449201ff31032ebd661c5484cb49ca
Gotfrid <gotfridwallenstein@gmail.com>
2023-02-02 18:30:55 +0300

    [#504882] - Find memory leak/3

--- src/main/java/ru/job4j/gc/leak/Post.java
+++ src/main/java/ru/job4j/gc/leak/Post.java
@@ -14,11 +14,11 @@ public class Post {
         this.comments = comments;
     }
 
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
