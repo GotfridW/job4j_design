55faa81f457cd951f094b8f00c8d1627bf371223
Gotfrid <gotfridwallenstein@gmail.com>
2023-12-28 00:53:25 +0300

    Method refactored using CyclicIterator

--- src/main/java/ru/job4j/iterator/Balancer.java
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -6,13 +6,9 @@ import java.util.List;
 
 public class Balancer {
     public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
-        nodes.forEach(node -> {
-            if (source.hasNext()) {
-                node.add(source.next());
-            }
-        });
-        if (source.hasNext()) {
-            split(nodes, source);
+        var iterator = new CyclicIterator<>(nodes);
+        while (source.hasNext()) {
+            iterator.next().add(source.next());
         }
     }
 }
