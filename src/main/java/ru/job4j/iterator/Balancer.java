41e7cd7ad41c6962f6ddfbfd31b06d90d0700d71
Gotfrid <gotfridwallenstein@gmail.com>
2023-12-27 21:37:36 +0300

    Method refactored.

--- src/main/java/ru/job4j/iterator/Balancer.java
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -6,14 +6,12 @@ import java.util.List;
 
 public class Balancer {
     public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
-        while (source.hasNext()) {
-            for (var node : nodes) {
-                if (source.hasNext()) {
-                    node.add(source.next());
-                } else {
-                    break;
-                }
+        nodes.forEach(node -> {
+            if (source.hasNext()) {
+                node.add(source.next());
             }
+        });
+        if (source.hasNext()) {
             split(nodes, source);
         }
     }
